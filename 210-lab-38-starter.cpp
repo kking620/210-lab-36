078b7ca7ed581573ff54e8e2ab8cef01b32d7ec4
Kristofer King <kking620@gmail.com>
2025-11-25 20:41:42 -0800

    Third commit

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -26,7 +26,50 @@ int main() {
 
     inputFile.close();
 
+    int choice;
+
+    while (choice != 5) {
+        menu();
+        
+        cout << "Please enter your choice: (1-5) ";
+        cin >> choice;
+
+        if (choice < 1 || choice > 5) {
+            cout << "Invalid input. Please try again.\n";
+        }
+        else {
+            switch(choice) {
+                case 1:
+                    addRecord(codesTree);
+                    break;
+                case 2:
+                    deleteRecord(codesTree);
+                    break;
+                case 3:
+                    searchRecord(codesTree);
+                    break;
+                case 4:
+                    modifyRecord(codesTree);
+                    break;
+                case 5:
+                    cout << "Goodbye!\n"; 
+                    break;
+            }
+        }
+    }
+
+
+
     codesTree.displayInOrder();
 
     return 0;
+}
+
+void menu() {
+    cout << "BST Menu:\n";
+    cout << "1. Add a new code\n";
+    cout << "2. Delete an existing code\n";
+    cout << "3. Search for a code\n";
+    cout << "4. Modify a code\n";
+    cout << "5. Exit\n";
 }

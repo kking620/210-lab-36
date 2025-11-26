13573c0615b8bfc5dec0f898fe3fd604411f3355
Kristofer King <kking620@gmail.com>
2025-11-25 20:07:44 -0800

    Milestone 1

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
+    IntBinaryTree tree;
+
+    tree.insertNode(50);
+    tree.insertNode(100);
     
+    cout << "Code set up using the provided files successfully!\n";
 
     return 0;
 }
